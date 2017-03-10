9f61b806112255f19c1c633c5023a26a14fa809f
bassaer <rf.1799@gmail.com>
2017-03-10 17:17:13 +0900

    Fix the picture bubble shape problem from Honeycomb to API 17

--- chatmessageview/src/main/java/com/github/bassaer/chatmessageview/views/RoundImageView.java
+++ chatmessageview/src/main/java/com/github/bassaer/chatmessageview/views/RoundImageView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -32,6 +33,13 @@ public class RoundImageView extends ImageView {
     }
 
     private void initClipPath() {
+
+        // Below Jelly Bean, clipPath on canvas would not work because lack of hardware acceleration
+        // support. Hence, we should explicitly say to use software acceleration.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2
+                && Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            setLayerType(LAYER_TYPE_SOFTWARE, null);
+        }
         mClipPath = new Path();
         RectF rect = new RectF(0, 0, getWidth(), getHeight());
         float radius = getResources().getDimensionPixelSize(R.dimen.view_radius_normal);
