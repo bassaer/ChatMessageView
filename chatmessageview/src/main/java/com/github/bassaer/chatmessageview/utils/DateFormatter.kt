527e60ffdd39d31ec84a3caeacb81d003619842a
Tsubasa Nakayama <app.nakayama@gmail.com>
2017-11-11 14:38:18 +0900

    convert DateFormatter.java into kotlin

---
+++ chatmessageview/src/main/java/com/github/bassaer/chatmessageview/utils/DateFormatter.kt
@@ -0,0 +1,13 @@
+package com.github.bassaer.chatmessageview.utils
+
+import java.util.*
+
+/**
+ * Date formatter of chat timeline separator.
+ * Created by nakayama on 2017/01/13.
+ */
+class DateFormatter : ITimeFormatter {
+    override fun getFormattedTimeText(createdAt: Calendar): String {
+        return TimeUtils.calendarToString(createdAt, "MMM. dd, yyyy")
+    }
+}
