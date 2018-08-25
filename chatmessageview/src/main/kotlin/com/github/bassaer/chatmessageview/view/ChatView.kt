af4dc3425f31fe580c5f47dc6082ac16aebf01a9
Maxim Paymushkin <maxim.paymushkin@gmail.com>
2018-08-25 03:52:23 +0300

    fix error: get non-null Drawable

--- chatmessageview/src/main/kotlin/com/github/bassaer/chatmessageview/view/ChatView.kt
+++ chatmessageview/src/main/kotlin/com/github/bassaer/chatmessageview/view/ChatView.kt
@@ -173,7 +173,7 @@ class ChatView : LinearLayout {
 
     private fun getColoredDrawable(color: Int, iconId: Int): Drawable {
         val colorStateList = ColorStateList.valueOf(color)
-        val icon = ContextCompat.getDrawable(context, iconId)
+        val icon = ContextCompat.getDrawable(context, iconId)!!
         val wrappedDrawable = DrawableCompat.wrap(icon)
         DrawableCompat.setTintList(wrappedDrawable, colorStateList)
         return wrappedDrawable
